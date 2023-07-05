df722f067bd4093b723c26abbeeb5cbf331145b9
yjhand12 <firdausanestaxyz@gmail.com>
2023-07-05 13:48:58 +0700

    Fix Dashboard Admin

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -13,7 +13,83 @@
 <div class="card mb-3">
     <div class="table-responsive" style="margin-top:10px; min-height:500px">
         <div class="mx-4">
-            <p style="font-size:22px;">Sistem Penerimaan Mahasiswa Baru Program Studi Rekayasa Keamanan Siber Politeknik Bhakti Semesta (STEPA)</p>
+            <p style="font-size:22px;">Sistem Penerimaan Mahasiswa Baru Program Studi Rekayasa Keamanan Siber Politeknik Bhakti Semesta (STEPA)</p><br>
+            <div class="row">
+                <div class="col-xl-4 col-md-6 mb-4">
+                    <div class="card border-left-success shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800"><a href="" class="text-secondary text-decoration-none">Data Alternatif</a></div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-users fa-2x text-gray-300"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-4 col-md-6 mb-4">
+                    <div class="card border-left-info shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800"><a href="" class="text-secondary text-decoration-none">Data Kriteria</a></div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-cube fa-2x text-gray-300"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-xl-4 col-md-6 mb-4">
+                    <div class="card border-left-primary shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800"><a href="" class="text-secondary text-decoration-none">Data Sub Kriteria</a></div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-cubes fa-2x text-gray-300"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-xl-4 col-md-6 mb-4">
+                    <div class="card border-left-secondary shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800"><a href="" class="text-secondary text-decoration-none">Data Penilaian</a></div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-edit fa-2x text-gray-300"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-xl-4 col-md-6 mb-4">
+                    <div class="card border-left-danger shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800"><a href="" class="text-secondary text-decoration-none">Data Hasil Akhir</a></div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-chart-area fa-2x text-gray-300"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
