20479d78b0dbe05006102175d6d8495ce85d2f4e
yjhand12 <firdausanestaxyz@gmail.com>
2023-07-14 13:41:02 +0700

    Fix Hasil Seleksi user

--- app/Http/Controllers/User/HasilSeleksiController.php
+++ app/Http/Controllers/User/HasilSeleksiController.php
@@ -9,81 +9,17 @@ use App\Models\Mahasiswa;
 use App\Models\Kriteria;
 use App\Models\SubKriteria;
 use App\Models\Nilai;
+use App\Models\Hasil;
 
 class HasilSeleksiController extends Controller
 {
     public function index()
     {
         $mahasiswa = Mahasiswa::get();
-        $kriteria = Kriteria::get();
-        $nilai = Nilai::select(
-        'nilai.id as id',
-        'mahasiswa.id as idmahasiswa',
-        'kriteria.id as idkriteria',
-        'sub_kriteria.id as idsubkriteria',
-        'mahasiswa.nama as nama',
-        'mahasiswa.nisn as nisn',
-        'kriteria.nama as kr',
-        'sub_kriteria.bobot as bobot',
-        'sub_kriteria.keterangan as keterangan'
-    )
-        ->leftJoin('mahasiswa', 'mahasiswa.id', '=', 'nilai.mahasiswa_id')
-        ->leftJoin('kriteria', 'kriteria.id', '=', 'nilai.kriteria_id')
-        ->leftJoin('sub_kriteria', 'sub_kriteria.id', '=', 'nilai.sub_kriteria_id')
-        ->get();
-
-    $nilaiak = Nilai::select(
-        'nilai.id as id',
-        'mahasiswa.id as idmahasiswa',
-        'kriteria.id as idkriteria',
-        'sub_kriteria.id as idsubkriteria',
-        'mahasiswa.nama as nama',
-        'mahasiswa.nisn as nisn',
-        'kriteria.nama as kr',
-        'sub_kriteria.bobot as bobot',
-        'sub_kriteria.keterangan as keterangan'
-    )
-        ->leftJoin('mahasiswa', 'mahasiswa.id', '=', 'nilai.mahasiswa_id')
-        ->leftJoin('kriteria', 'kriteria.id', '=', 'nilai.kriteria_id')
-        ->leftJoin('sub_kriteria', 'sub_kriteria.id', '=', 'nilai.sub_kriteria_id')
-        ->get();
-
-    // Normalization
-    foreach ($mahasiswa as $mhs) {
-        $mhsfilter = $nilai->where('idmahasiswa', $mhs->id)->values()->all();
-
-        // Skip mahasiswa yang tidak memiliki nilai
-        if (count($mhsfilter) === 0) {
-            continue;
-        }
-
-        $rates = [];
-
-        foreach ($kriteria as $ik => $k) {
-            $rates = $nilaiak->map(function ($item) use ($k) {
-                if ($k->id == $item->idkriteria) {
-                    return $item->bobot;
-                }
-            })->toArray();
-
-            $rates = array_values(array_filter($rates));
-
-            if ($k->sifat == 'Benefit') {
-                $result = $mhsfilter[$ik]->bobot / max($rates);
-                $msg = 'rate ' . $mhsfilter[$ik]->bobot . ' max ' . max($rates) . ' res ' . $result;
-            } elseif ($k->sifat == 'Cost') {
-                $result = min($rates) / $mhsfilter[$ik]->bobot;
-            }
-            $result *= $k->bobot;
-            $mhsfilter[$ik]->bobot = round($result, 2);
-        }
-    }
-
-    return view('pages.user.hasil', [
-        'mahasiswa' => $mahasiswa,
-        'kriteria' => $kriteria,
-        'nilai' => $nilai
-    ])->with('i', 0);
-        
+        $hasil = Hasil::all();
+        return view('pages.user.hasil', [
+            'mahasiswa' => $mahasiswa,
+            'hasil' => $hasil,
+        ]);
     }
 }
--- resources/views/pages/user/hasil.blade.php
+++ resources/views/pages/user/hasil.blade.php
@@ -19,8 +19,8 @@
 
 <section id="services">
     <div class="container">
-            <div class="col-lg-12 mx-auto text-center mb-4">
-                <h5 class="fw-light fs-3 fs-lg-5 lh-sm mb-3">Hasil Seleksi Penerimaan Mahasiswa Baru Program Studi<br>Rekayasa Keamanan Siber</h5>
+        <div class="col-lg-12 mx-auto text-center mb-4">
+            <h5 class="fw-light fs-3 fs-lg-5 lh-sm mb-3">Hasil Seleksi Penerimaan Mahasiswa Baru Program Studi<br>Rekayasa Keamanan Siber</h5>
                 <div class="card mb-3 mt-5">
                     <div class="table-responsive px-3 pb-3" style="margin-top:10px">
                         <table id="dataTablesHasil" class="table align-items-center table-hover table-bordered">
@@ -29,59 +29,29 @@
                                 <th>No</th>
                                 <th>NISN</th>
                                 <th>Nama</th>
+                                <th>Jenis Kelamin</th>
                                 <th>Asal Sekolah</th>
                                 <th>Nilai Akhir</th>
                                 <th>Keputusan</th>
                             </tr>
                         </thead>
                         <tbody>
-                            @php
-                            $sortedMahasiswa = $mahasiswa->sortByDesc(function ($mhs) use ($nilai) {
-                                $nle = $nilai->where('idmahasiswa', $mhs->id)->all();
-                                $total = 0;
-
-                                foreach ($nle as $n) {
-                                    $total += $n->bobot * 100;
-                                }
-
-                                return [$total, -$mhs->id];
-                            });
-
-                            $totalMahasiswa = count($mahasiswa);
-                            $jumlahTopMahasiswa = ceil($totalMahasiswa / 2);
-                            $topMahasiswa = $sortedMahasiswa->take($jumlahTopMahasiswa)->pluck('id')->all();
-                            @endphp
-
-                            @foreach ($mahasiswa as $mhs)
+                            @foreach ($hasil as $hasil)
                             <tr>
                                 <td>{{ $loop->iteration }}</td>
-                                <td>{{ $mhs->nisn }}</td>
-                                <td>{{ $mhs->nama }}</td>
-                                <td>{{ $mhs->asal_sekolah }}</td>
-                                @php
-                                $nle = $nilai->where('idmahasiswa', $mhs->id)->all();
-                                $total = 0;
-
-                                foreach ($nle as $n) {
-                                    $total += $n->bobot * 100;
-                                }
-                                @endphp
-                                <td>{{ $total }}</td>
-                                <td>
-                                    @if (in_array($mhs->id, $topMahasiswa))
-                                        <b>DITERIMA</b>
-                                    @else
-                                        <b>TIDAK DITERIMA</b>
-                                    @endif
-                                </td>
+                                <td>{{ $hasil->mahasiswa->nisn }}</td>
+                                <td>{{ $hasil->mahasiswa->nama }}</td>
+                                <td>{{ $hasil->mahasiswa->jenis_kelamin }}</td>
+                                <td>{{ $hasil->mahasiswa->asal_sekolah }}</td>
+                                <td>{{ $hasil->nilai }}</td>
+                                <td>{{ $hasil->keputusan }}</td>
                             </tr>
                             @endforeach
                         </tbody>
                         </table>
                     </div>
                 </div>
-            </div>
-
+        </div>
     </div>
 </section>
 
